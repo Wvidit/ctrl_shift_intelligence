fd546f1ae9c497120bddbe8f6d2b6f99ae112475
Vidit <nahbro2511@gmail.com>
2025-02-20 19:55:04 +0530

    Update Customer_support.py
    Complete script for customer support.

--- Business/Customer_support.py
+++ Business/Customer_support.py
@@ -4,10 +4,9 @@ from openai import OpenAI
 #For testing purposes we are using product name(assuming they are distinct)
 def customer_support(context):
     model = OpenAI(
-        base_url="https://openrouter.ai/api/v1",
-        api_key="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",
+        base_url = "https://openrouter.ai/api/v1",
+        api_key = "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",
     )
-
     messages = [
         {"role": "system", "content": '''Check if prompt have the following information or the following information can be deduced from it: 
                                         1)Product id or Product name
@@ -23,10 +22,13 @@ def customer_support(context):
     if response.choices[0].message.content == '1':
 
         message2 = [
-            {"role": "system", "content": '''You are a customer support executive, provide customer support for Product name given if bill id is mentioned.'''}
+            {"role": "system", "content": "You are an expert for the product provided by the user."},
         ]
-
-        response = model.chat.completions.create(
+        message2.append({"role": "user", "content": context })
+        response1 = model.chat.completions.create(
             model = "gpt-3.5-turbo",
             messages = message2
         )
+        return response1.choices[0].message.content
+    else:
+        return "Please provide Product id or Product name and Bill id"
